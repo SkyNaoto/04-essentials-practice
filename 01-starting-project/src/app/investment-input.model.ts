8b7b1f92932e0f9d3c17802a658627cbb2d5f64a
SkyNaoto <naoto_70102@yahoo.co.jp>
2025-08-09 10:44:25 +0900

    Finished 82

---
+++ 01-starting-project/src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestmentInput {
+    initialInvestment:number, 
+    duration:number, 
+    expectedReturn:number,
+    annualInvestment:number
+  }
+
+//   export type InvestmentInputOutput = {
+//     initialInvestment:number, 
+//     duration:number, 
+//     expectedReturn:number,
+//     annualInvestment:number
+//   };
--- 01-starting-project/src/app/user-input/user-input.component.ts
+++ 01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,18 +10,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<{
-    initialInvestment:number, 
-    duration:number, 
-    expectedReturn:number,
-    annualInvestment:number
-  }>();
-  // @Output() calculate = new EventEmitter<{
-  //   initialInvestment:number, 
-  //   duration:number, 
-  //   expectedReturn:number,
-  //   annualInvestment:number
-  // }>();
+  calculate = output<InvestmentInput>();
+  // @Output() calculate = new EventEmitter<InvestmentInput>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
